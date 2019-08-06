16d005fb6e3809a652a8637b95edab8de5401485
root <christinecyueh@gmail.com>
2019-08-06 14:15:10 -0400

    completed weather app; TODO: add racer/animal pages and firebase

--- src/components/weatherInfo/index.js
+++ src/components/weatherInfo/index.js
@@ -3,14 +3,26 @@ import './index.css';
 
 class WeatherInfo extends Component {
   render() {
+    let data = this.props.data;
     return (
       <div className="row">
         <div className="col-md-12">
-          <h3>City: Boston</h3>
-          <h3>Country: US</h3>
-          <h3>Temperature: 87&#8457;</h3>
-          <h3>Humidity: 34%</h3>
-          <h3>Description: Cloudy with a chance of meatballs.</h3>
+          {/* below is an if statement saying if data exists, then show info; must return one element from statement (thus wrapping <h3>'s in <div)*/}
+          { data.name &&
+            <div>
+              <h3>City: {data.name}</h3>
+              <h3>Country: {data.sys.country}</h3>
+              <h3>Temperature: {data.main.temp.toFixed(0)}&#8457;</h3>
+              <h3>Humidity: {data.main.humidity}%</h3>
+              <h3>Description: {data.weather[0].main}</h3>
+            </div>
+          }
+
+          {/* error message will appear if city doesn't exist */}
+          {
+            data.cod === '404' &&
+            <h3>Error: {data.message}</h3>
+          }
         </div>
       </div>
     );
